bd7ca9d9a52dceac55ed4fad3c7ede7f01333ea2
Merayven <mpopiel@tuta.io>
2024-04-09 07:46:21 +0200

    Added snippets data to display on the template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,14 +12,11 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	// snippets, err := app.snippets.Latest()
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 
-	// for _, snippet := range snippets {
-	// 	fmt.Fprintf(w, "%+v\n", snippet)
-	// }
 	files := []string{
 		"./ui/html/base.html",
 		"./ui/html/pages/home.html",
@@ -34,6 +31,15 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+
+	data := templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
