4404a56f2cac3bae0611d64da157ca8b1862a4ea
Merayven <mpopiel@tuta.io>
2024-04-09 13:41:33 +0200

    Changed rendering templates with newTemplateData func (default data Currentyear, with snippet slice added to the data)

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,9 +16,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, r, err)
 	}
 
-	app.render(w, r, http.StatusOK, "home.html", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w, r, http.StatusOK, "home.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -39,9 +40,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "view.html", templateData{
-		Snippet: snippet,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.html", data)
 
 }
 
