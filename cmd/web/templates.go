9b677a82b64b0312dee4addc7077c1ec5ab0c6a6
Merayven <mpopiel@tuta.io>
2024-04-18 11:38:39 +0200

    Serving embedded html templates using server FS

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
+
+	"bitsnipp.merayven.net/ui"
 
 	"bitsnipp.merayven.net/internal/models"
 )
@@ -29,7 +32,7 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
@@ -37,17 +40,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.html",
+			"html/common/*.html",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/common/*.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
